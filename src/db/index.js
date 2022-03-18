dac990e36fa3b6648229b9afcd3e49397030454e
martin835 <64438132+martin835@users.noreply.github.com>
2022-03-18 16:36:40 +0100

    fixing ssl

--- src/db/index.js
+++ src/db/index.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
     dialectOptions: {
       ssl: {
         required: true,
-        rejectUnAuthorized: false,
+        rejectUnauthorized: false,
       },
     },
   }),
